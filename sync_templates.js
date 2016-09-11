eaef827f24ca9e3d201f2c1322e41903b92051d5
Ravindra Kumar <ravidsrk@gmail.com>
2016-09-11 14:17:08 +0530

    Initial setup with sync js

---
+++ sync_templates.js
@@ -0,0 +1,125 @@
+'use strict';
+
+
+const rimraf = require('rimraf');
+const path = require('path');
+const mv = require('mv');
+const mkdirp = require('mkdirp');
+const clone = require("nodegit").Clone;
+const replace = require("replace");
+const ncp = require('ncp').ncp;
+const Finder = require('fs-finder');
+const deferred = require('deferred');
+
+// Clone a given repository into the `./tmp` folder.
+rimraf.sync(__dirname + '/templates')
+rimraf.sync(__dirname + '/tmp')
+mkdirp('./templates')
+
+clone("https://github.com/googlesamples/android-architecture", "./tmp")
+  .then(function(repo) {
+    checkOutAndCopy(repo, "todo-mvp")(function(success) {
+      checkOutAndCopy(repo, "todo-mvp-dagger")(function(success) {
+        checkOutAndCopy(repo, "todo-mvp-loaders")(function(success) {
+          checkOutAndCopy(repo, "todo-mvp-clean")(function(success) {
+            checkOutAndCopy(repo, "todo-mvp-rxjava")(function(success) {
+              checkOutAndCopy(repo, "todo-mvp-contentproviders")(function(success) {
+                checkOutAndCopy(repo, "todo-databinding")(function(success) {
+                  rimraf.sync(__dirname + '/tmp')
+                  console.log('success is', success);
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+function checkOutAndCopy(repo, name) {
+  let def = deferred();
+  repo.getBranch('refs/remotes/origin/' + name)
+    .then(function(reference) {
+      console.log("Checking out branch " + name);
+      return repo.checkoutRef(reference);
+    })
+    .then(function() {
+      replaceAndRename(true);
+      copyFiles(name, def);
+    });
+  return def.promise;
+}
+
+function copyFiles(name, def) {
+  console.log("Copying files to ./templates/" + name);
+
+  ncp('./tmp/todoapp', './templates/' + name, function(err) {
+    if (err) {
+      console.error(err);
+      def.reject('error');
+    } else {
+      replaceAndRename(false);
+      console.log('Renaming files to add .ftl extension!');
+      // renameFileToFtl(name);
+      def.resolve('Copying complete!');
+    }
+  });
+}
+
+function renameFileToFtl(name) {
+  Finder.from("./templates/" + name).findFiles('*.java', function(files) {
+        for (var i = 0; i < files.length; i++) {
+          mv(files[i], files[i]+'.ftl', function (err) {
+            if (err) {
+              return console.log(err);
+            }
+          });
+        };
+      });
+
+      Finder.from("./templates/" + name).findFiles('*.xml', function(files) {
+        for (var i = 0; i < files.length; i++) {
+          mv(files[i], files[i]+'.ftl', function (err) {
+            if (err) {
+              return console.log(err);
+            }
+          });
+        };
+      });
+}
+
+function replaceAndRename(toggle) {
+  let dotGitignore = "./tmp/todoapp/.gitignore";
+  let gitIgnore = "./tmp/todoapp/gitignore";
+  let appDotGitignore = "./tmp/todoapp/app/.gitignore";
+  let appGitIgnore = "./tmp/todoapp/app/gitignore";
+
+  replace({
+    regex: toggle ? "com.example.android.architecture.blueprints.todoapp" : "${packageName}",
+    replacement: toggle ? "${packageName}" : "com.example.android.architecture.blueprints.todoapp",
+    paths: ['./tmp/todoapp'],
+    recursive: true,
+    silent: true,
+  });
+
+  mv(toggle ? dotGitignore : gitIgnore, toggle ? gitIgnore : dotGitignore, function(err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      let file = toggle ?  ".gitignore" : "gitignore";
+      return console.log("Renamed root folder " + file);
+    }
+  });
+
+  mv(toggle ? appDotGitignore : appGitIgnore, toggle ? appGitIgnore : appDotGitignore, function(err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      let file = toggle ?  ".gitignore" : "gitignore";
+      return console.log("Renamed app folder " + file);
+    }
+  });
+}
